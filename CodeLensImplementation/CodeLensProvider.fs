980ff9089acc67dd9e4ec47d900eaa276bd806b0
realvictorprm <mueller.vpr@gmail.com>
2017-09-23 21:34:27 +0200

    Initial commit.
    Adds Code-Lens code which is the same of the current Code Lens PR for Visual F#.
    Please be aware of that part of the code is from Visual F# extracted.
    
    Signed-off-by: realvictorprm <mueller.vpr@gmail.com>

---
+++ CodeLensImplementation/CodeLensProvider.fs
@@ -0,0 +1,149 @@
+﻿// Copyright (c) 2017 Victor Peter Rouven Müller and Microsoft (Visual F#, MIT License)
+// Licensed under the Apache
+namespace rec CodeLens
+
+open System
+open System.Windows.Media
+open Microsoft.VisualStudio.Text
+open Microsoft.VisualStudio.Text.Editor
+open Microsoft.VisualStudio.Text.Formatting
+open System.ComponentModel.Composition
+open System.Threading
+open Microsoft.VisualStudio
+open System.Windows
+open System.Collections.Generic
+open Microsoft.VisualStudio.Text.Tagging
+open System.Collections.Concurrent
+open System.Collections
+open System.Globalization
+open System.Windows.Controls
+open Microsoft.VisualStudio.Utilities
+open Microsoft.VisualStudio.Shell
+open Microsoft.VisualStudio.TextManager.Interop
+open System.Windows.Media.Animation
+
+type SampleTagger (view, buffer) as self =
+    inherit CodeLensGeneralTagger(view, buffer)
+
+    let collectLinesWhichContain (snapshot:ITextSnapshot) word =
+        snapshot.Lines
+        |> Seq.filter(fun line -> line.GetText().Contains word)
+        |> Seq.toArray
+
+        
+    do (
+            (* Single code lens test
+            buffer.Insert(0, "Foo") |> ignore
+            let snapshot = buffer.CurrentSnapshot
+            let trackingSpan = snapshot.CreateTrackingSpan(1, 3, SpanTrackingMode.EdgeInclusive)
+            let uiElement = self.AddCodeLens trackingSpan
+
+            let textBox = new TextBox(Text = "Hello", Width = 100., Height = 20.)
+
+            let animation = 
+                DoubleAnimation(
+                    To = Nullable 1.,
+                    Duration = (TimeSpan.FromMilliseconds 500. |> Duration.op_Implicit),
+                    EasingFunction = QuadraticEase()
+                    )
+            let sb = Storyboard()
+            sb.Children.Add animation
+            textBox.Opacity <- 0.
+            Storyboard.SetTarget(sb, textBox)
+            Storyboard.SetTargetProperty(sb, PropertyPath Control.OpacityProperty)
+            uiElement.IsVisibleChanged
+            |> Event.filter (fun eventArgs -> eventArgs.NewValue :?> bool)
+            |> Event.add (fun _ ->
+                    sb.Begin()
+                )
+            self.AddUiElementToCodeLensOnce trackingSpan textBox 
+            *)
+
+            /// Responsive code lens test
+            buffer.Changed.Add(fun event ->
+                let snapshot = event.After
+                let linesToTag = collectLinesWhichContain snapshot "TODO"
+                let lineNumbersToTag = 
+                    linesToTag 
+                    |> Array.map(fun line -> line.LineNumber) 
+                    |> HashSet
+
+                let currentCodeLens = self.CurrentCodeLens
+
+                let codeLensToContinue, codeLensToRemove = 
+                    currentCodeLens
+                    |> Seq.toArray
+                    |> Array.partition(fun pair -> pair.Key |> lineNumbersToTag.Contains)
+                
+                let codeLensToAdd =
+                    let continuedLineNumbers = 
+                        codeLensToContinue 
+                        |> Seq.map (fun pair -> pair.Key)
+                        |> HashSet
+                    lineNumbersToTag
+                    |> Seq.toArray
+                    |> Array.filter (continuedLineNumbers.Contains >> not)
+                
+                for codeLens in codeLensToRemove do
+                    let trackingSpans = codeLens.Value |> Seq.toArray
+                    for trackingSpan in trackingSpans do
+                        self.RemoveCodeLens trackingSpan |> ignore
+
+                for lineNumber in codeLensToAdd do
+                    let line = snapshot.GetLineFromLineNumber lineNumber
+                    let trackingSpan = snapshot.CreateTrackingSpan(Span(line.Start.Position, line.Length), SpanTrackingMode.EdgeExclusive)
+                    let uiElement = self.AddCodeLens trackingSpan
+                    
+                    let textBox = new TextBlock(Text = "Hello", Width = 100., Height = 20.)
+
+                    let animation = 
+                        DoubleAnimation(
+                            To = Nullable 1.,
+                            Duration = (TimeSpan.FromMilliseconds 500. |> Duration.op_Implicit),
+                            EasingFunction = QuadraticEase()
+                            )
+                    let sb = Storyboard()
+                    sb.Children.Add animation
+                    textBox.Opacity <- 0.
+                    Storyboard.SetTarget(sb, textBox)
+                    Storyboard.SetTargetProperty(sb, PropertyPath Control.OpacityProperty)
+                    uiElement.IsVisibleChanged
+                    |> Event.filter (fun eventArgs -> eventArgs.NewValue :?> bool)
+                    |> Event.add (fun _ ->
+                            sb.Begin()
+                        )
+                    self.AddUiElementToCodeLensOnce trackingSpan textBox
+                    //uiElement.IsVisibleChanged
+                    //|> Event.filter (fun eventArgs -> eventArgs.NewValue :?> bool)
+                    //|> Event.add (fun _ ->
+                    //    let textBox = new TextBox(Text = "Hello", Width = 100., Height = 20.)
+                    //    self.AddUiElementToCodeLensOnce trackingSpan textBox
+                    //)
+            )
+        )
+
+[<Export(typeof<IViewTaggerProvider>)>]
+[<TagType(typeof<CodeLensGeneralTag>)>]
+[<ContentType("text")>]
+[<TextViewRole(PredefinedTextViewRoles.Document)>]
+type internal CodeLensProvider  
+    [<ImportingConstructor>]
+    () =
+
+    [<Export(typeof<AdornmentLayerDefinition>); Name("CodeLens");
+      Order(Before = PredefinedAdornmentLayers.Text);
+      TextViewRole(PredefinedTextViewRoles.Document)>]
+    member val CodeLensAdornmentLayerDefinition : AdornmentLayerDefinition = null with get, set
+
+    interface IViewTaggerProvider with
+        override __.CreateTagger(view, buffer) = 
+            let wpfView =
+                match view with
+                | :? IVsUserData as userData ->
+                    let res, v = userData.GetData (ref Microsoft.VisualStudio.Editor.DefGuidList.guidIWpfTextViewHost)
+                    
+                    (v :?> IWpfTextViewHost).TextView
+                | :? IWpfTextView as view -> view
+                | _ -> failwith "error"
+            
+            box(SampleTagger(wpfView, buffer)) :?> _
---
+++ CodeLensImplementation/Utils.fs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) 2017 Victor Peter Rouven Müller and Microsoft (Visual F#, MIT License)
+// Licensed under the Apache
+
+[<AutoOpen>]
+module Utils 
+open System.Diagnostics
+open System
+
+[<Sealed>]
+type AsyncMaybeBuilder () =
+    [<DebuggerStepThrough>]
+    member __.Return value : Async<'T option> = Some value |> async.Return
+
+    [<DebuggerStepThrough>]
+    member __.ReturnFrom value : Async<'T option> = value
+
+    [<DebuggerStepThrough>]
+    member __.ReturnFrom (value: 'T option) : Async<'T option> = async.Return value
+
+    [<DebuggerStepThrough>]
+    member __.Zero () : Async<unit option> =
+        Some () |> async.Return
+
+    [<DebuggerStepThrough>]
+    member __.Delay (f : unit -> Async<'T option>) : Async<'T option> = async.Delay f
+
+    [<DebuggerStepThrough>]
+    member __.Combine (r1, r2 : Async<'T option>) : Async<'T option> =
+        async {
+            let! r1' = r1
+            match r1' with
+            | None -> return None
+            | Some () -> return! r2
+        }
+
+    [<DebuggerStepThrough>]
+    member __.Bind (value: Async<'T option>, f : 'T -> Async<'U option>) : Async<'U option> =
+        async {
+            let! value' = value
+            match value' with
+            | None -> return None
+            | Some result -> return! f result
+        }
+
+    [<DebuggerStepThrough>]
+    member __.Bind (value: System.Threading.Tasks.Task<'T>, f : 'T -> Async<'U option>) : Async<'U option> =
+        async {
+            let! value' = Async.AwaitTask value
+            return! f value'
+        }
+
+    [<DebuggerStepThrough>]
+    member __.Bind (value: 'T option, f : 'T -> Async<'U option>) : Async<'U option> =
+        async {
+            match value with
+            | None -> return None
+            | Some result -> return! f result
+        }
+
+    [<DebuggerStepThrough>]
+    member __.Using (resource : ('T :> IDisposable), body : _ -> Async<_ option>) : Async<_ option> =
+        try body resource
+        finally if not (isNull resource) then resource.Dispose ()
+
+    [<DebuggerStepThrough>]
+    member x.While (guard, body : Async<_ option>) : Async<_ option> =
+        if guard () then
+            x.Bind (body, (fun () -> x.While (guard, body)))
+        else
+            x.Zero ()
+
+    [<DebuggerStepThrough>]
+    member x.For (sequence : seq<_>, body : 'T -> Async<unit option>) : Async<_ option> =
+        x.Using (sequence.GetEnumerator (), fun enum ->
+            x.While (enum.MoveNext, x.Delay (fun () -> body enum.Current)))
+
+    [<DebuggerStepThrough>]
+    member inline __.TryWith (computation : Async<'T option>, catchHandler : exn -> Async<'T option>) : Async<'T option> =
+            async.TryWith (computation, catchHandler)
+
+    [<DebuggerStepThrough>]
+    member inline __.TryFinally (computation : Async<'T option>, compensation : unit -> unit) : Async<'T option> =
+            async.TryFinally (computation, compensation)
+
+let asyncMaybe = AsyncMaybeBuilder()
+
+let inline liftAsync (computation : Async<'T>) : Async<'T option> =
+    async {
+        let! a = computation
+        return Some a 
+    }
+
+/// Assert helpers
+type internal Assert() = 
+    /// Display a good exception for this error message and then rethrow.
+    static member Exception(e:Exception) =  
+        System.Diagnostics.Debug.Assert(false, "Unexpected exception seen in code lens", e.ToString())
+
+let StartAsyncSafe cancellationToken computation =
+    let computation =
+        async {
+            try
+                return! computation
+            with e ->
+                Assert.Exception(e)
+                return Unchecked.defaultof<_>
+        }
+    Async.Start (computation, cancellationToken)
